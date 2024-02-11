9120b152d5849480479f814c4fe04dc09849a4b3
himza25 <himza.of.theking95@gmail.com>
2024-02-10 20:46:02 -0800

    update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,44 +1,14 @@
-$(document).ready(() => {
-  let amenities = {};
-  $('input[type="checkbox"]').change(() => {
-    let checkBoxs = $('input[type = "checkbox"]')
-    for (let i = 0; i < checkBoxs.length; i++) {
-      let key = $(checkBoxs[i]).attr('data-id');
-      if ($(checkBoxs[i]).prop('checked')) {
-        if (!(key in amenities)) {
-          amenities[key] = $(checkBoxs[i]).attr('data-name');
-        }
-      } else {
-        if (key in amenities) {
-          delete amenities[key];
-        }
-      }
+$(document).ready(init);
+
+function init () {
+  const amenityObj = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityObj[$(this).attr('data-name')];
     }
-    let amenText = $(".amenities h4");
-    amenText.css("text-overflow", "ellipsis");
-    amenText.css("white-space", "nowrap");
-    amenText.html("&nbsp;");
-    initWidth = amenText.width();
-    let i = 0, b = 0;
-    for (let key in amenities) {
-      if ($(".amenities h4").width() > initWidth) {
-        amenText.append('...');
-        break;
-      }
-      if (i >= 1) {
-        amenText.append(", ");
-      }
-      amen = amenities[key]
-      for (let j = 0; j < amen.length; j++) {
-        if ($(".amenities h4").width() > initWidth) {
-          amenText.append('...');
-          b = 1;
-          break;
-        }
-        amenText.append(amen[j]);
-      }
-      if (b) { break; }
-      i++;
-    }
-  })
-})
+    const names = Object.keys(amenityObj);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+}
