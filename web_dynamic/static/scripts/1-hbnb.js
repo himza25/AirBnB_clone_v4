112cb6053886e37f78c194380a369d2ce6403392
himza25 <himza.of.theking95@gmail.com>
2024-02-10 21:21:52 -0800

    update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,15 @@
-<div class="amenities">
-    <h3>Amenities</h3>
-    <h4>&nbsp;</h4>
-    <div class="popover">
-        <ul>
-            {% for amenity in amenities %}
-            <li>
-                <input type="checkbox" data-id="{{ amenity.id }}" data-name="{{ amenity.name }}"> {{ amenity.name }}
-            </li>
-            {% endfor %}
-        </ul>
-    </div>
-</div>
+/* global $ */
+$(document).ready(function () {
+  const checkedAmenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+
+    const amenityNames = Object.values(checkedAmenities);
+    $('div.amenities > h4').text(amenityNames.join(', '));
+  });
+});
